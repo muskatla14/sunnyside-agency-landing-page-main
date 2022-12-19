34cc7c3ac155e41cb7b3289e33c3858b1bc8b413
Marko Robovic <markorobovic02@gmail.com>
2022-12-19 14:35:25 +0100

    add readme

---
+++ script/app.js
@@ -0,0 +1,6 @@
+
+const nav = document.querySelector('.showcase-navbar ul');
+const burger = document.querySelector('.hamburger');
+document.querySelector('.hamburger').addEventListener('click', () => {
+    nav.classList.toggle('active');
+})  
